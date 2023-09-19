82706ceedceccccf89644be33ddd77539ce2750b
ThongDTH <thongdo114@gmail.com>
2023-09-19 20:36:28 +0700

    Imp GenericRepo

---
+++ DataAccess/Implementation/OrderDetailRepository.cs
@@ -0,0 +1,15 @@
+﻿using BusinessObject.Models;
+using DataAccess.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Implementation
+{
+    public class OrderDetailRepository : GenericRepository<OrderDetail> , IOrderDetailRepository
+    {
+        public OrderDetailRepository(FstoreDbContext context) : base(context) { }
+    }
+}
---
+++ DataAccess/Implementation/ProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using BusinessObject.Models;
+using DataAccess.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Implementation
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(FstoreDbContext context) : base(context) { }
+    }
+}
