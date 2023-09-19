e0c6578c7122b909dddeaec29260937fcad3bc1b
ThongDTH <thongdo114@gmail.com>
2023-09-19 19:45:54 +0700

    Initial commit

---
+++ BusinessObject/Models/FstoreDbContext.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessObject.Models;
+
+public partial class FstoreDbContext : DbContext
+{
+    public FstoreDbContext()
+    {
+    }
+
+    public FstoreDbContext(DbContextOptions<FstoreDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Member> Members { get; set; }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=(local);Database=FStoreDB;uid=sa;pwd=123;Trusted_Connection =true;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PK__Category__19093A0B9B67E63E");
+
+            entity.ToTable("Category");
+
+            entity.Property(e => e.CategoryId).ValueGeneratedNever();
+            entity.Property(e => e.CategoryName)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Member>(entity =>
+        {
+            entity.HasKey(e => e.MemberId).HasName("PK__Member__0CF04B182CBAC6E3");
+
+            entity.ToTable("Member");
+
+            entity.Property(e => e.MemberId).ValueGeneratedNever();
+            entity.Property(e => e.City)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+            entity.Property(e => e.CompanyName)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.Country)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Password)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.OrderId).HasName("PK__Order__C3905BCF835A7703");
+
+            entity.ToTable("Order");
+
+            entity.Property(e => e.OrderId).ValueGeneratedNever();
+            entity.Property(e => e.Freight).HasColumnType("money");
+            entity.Property(e => e.OrderDate).HasColumnType("datetime");
+            entity.Property(e => e.RequiredDate).HasColumnType("datetime");
+            entity.Property(e => e.ShippedDate).HasColumnType("datetime");
+
+            entity.HasOne(d => d.Member).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.MemberId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK__Order__MemberId__4BAC3F29");
+        });
+
+        modelBuilder.Entity<OrderDetail>(entity =>
+        {
+            entity.HasKey(e => new { e.OrderId, e.ProductId }).HasName("PK__OrderDet__08D097A3AA7F890E");
+
+            entity.ToTable("OrderDetail");
+
+            entity.Property(e => e.UnitPrice).HasColumnType("money");
+
+            entity.HasOne(d => d.Order).WithMany(p => p.OrderDetails)
+                .HasForeignKey(d => d.OrderId)
+                .HasConstraintName("FK__OrderDeta__Order__534D60F1");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.OrderDetails)
+                .HasForeignKey(d => d.ProductId)
+                .HasConstraintName("FK__OrderDeta__Produ__5441852A");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.ProductId).HasName("PK__Product__B40CC6CDA94C616D");
+
+            entity.ToTable("Product");
+
+            entity.Property(e => e.ProductId).ValueGeneratedNever();
+            entity.Property(e => e.ProductName)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.UnitPrice).HasColumnType("money");
+            entity.Property(e => e.Weight)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products)
+                .HasForeignKey(d => d.CategoryId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK__Product__Categor__5070F446");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DataAccess/GenericRepository.cs
@@ -0,0 +1,56 @@
+﻿using BusinessObject.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected FstoreDbContext _context = null;
+        protected DbSet<T> dbSet = null;
+
+        public GenericRepository()
+        {
+            _context = new FstoreDbContext();
+            dbSet = _context.Set<T>();
+        }
+
+        public GenericRepository(FstoreDbContext dbContext)
+        {
+            _context = dbContext;
+            dbSet = dbContext.Set<T>();
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return dbSet.ToList();
+        }
+        public T GetById(object id)
+        {
+            return dbSet.Find(id);
+        }
+        public void Insert(T obj)
+        {
+            dbSet.Add(obj);
+        }
+
+        public void Update(T obj)
+        {
+            dbSet.Attach(obj);
+            _context.Entry(obj).State = EntityState.Modified;
+        }
+        public void Delete(object id)
+        {
+            T existing = dbSet.Find(id);
+            dbSet.Remove(existing);
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
